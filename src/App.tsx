052daf056e0392a2baf5475927b1938bc651960a
崔文辉 <huiazir@qq.com>
2020-03-24 09:38:12 +0800

    fix:修复代码风格

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Azir Design
-      </header>
+      <header className="App-header">Azir Design</header>
     </div>
   );
 }
