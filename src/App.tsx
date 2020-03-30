62a93f5ff1c250628823892120c00bc84a50715a
崔文辉 <huiazir@qq.com>
2020-03-30 10:01:12 +0800

    新增Grid栅格布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ButtonExample from "./components/Button/example";
 import ProgressExample from "./components/Progress/example";
-
+import GridExample from "./components/Grid/example";
 function App() {
   return (
     <div className="App">
       <header className="App-header">Azir Design</header>
       <ButtonExample />
       <ProgressExample />
+      <GridExample />
     </div>
   );
 }
