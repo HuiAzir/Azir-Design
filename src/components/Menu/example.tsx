aa2d9bc2cf7607f48a5e6f4b9173ff6c3d3d91bc
崔文辉 <huiazir@qq.com>
2020-04-20 01:55:47 +0800

    Menu组件测试更新

--- src/components/Menu/example.tsx
+++ src/components/Menu/example.tsx
@@ -2,13 +2,34 @@ import React from 'react';
 import Menu from './menu';
 import MenuItem from './menuItem';
 const Example = () => {
+  const data = [
+    'MenuItem----1',
+    'MenuItem----2',
+    'MenuItem----3',
+    'MenuItem----4'
+  ];
   return (
-    <Menu>
-      <MenuItem>MenuItem----1</MenuItem>
-      <MenuItem>MenuItem----2</MenuItem>
-      <MenuItem>MenuItem----3</MenuItem>
-      <MenuItem>MenuItem----4</MenuItem>
-    </Menu>
+    <>
+      <Menu defaultIndex={0} onSelect={index => console.log(index)}>
+        {data.map((item, index) => (
+          <MenuItem key={index} index={index} disable={index === 1}>
+            {item}
+          </MenuItem>
+        ))}
+      </Menu>
+      <Menu
+        mode="vertical"
+        defaultIndex={0}
+        onSelect={index => console.log(index)}
+        style={{ width: 200 }}
+      >
+        {data.map((item, index) => (
+          <MenuItem key={index} index={index} disable={index === 1}>
+            {item}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
   );
 };
 
